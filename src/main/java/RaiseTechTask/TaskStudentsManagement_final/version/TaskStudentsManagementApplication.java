8ece842560fa2c1dd499949ca13ddb5bac016648
Shinya <rikutoweb5@gmail.com>
2025-04-27 02:00:48 +0900

    Initial commit

---
+++ src/main/java/RaiseTechTask/TaskStudentsManagement_final/version/TaskStudentsManagementApplication.java
@@ -0,0 +1,13 @@
+package RaiseTechTask.TaskStudentsManagement_final.version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskStudentsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskStudentsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RaiseTechTask/TaskStudentsManagement_final/version/TaskStudentsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package RaiseTechTask.TaskStudentsManagement_final.version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskStudentsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
