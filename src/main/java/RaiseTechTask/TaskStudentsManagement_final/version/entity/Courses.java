71ca4ffb8b336b4d1f69657fff31cedc1297aa5c
Shinya <rikutoweb5@gmail.com>
2025-05-01 15:23:59 +0900

    add lombox

--- src/main/java/RaiseTechTask/TaskStudentsManagement_final/version/entity/Courses.java
+++ src/main/java/RaiseTechTask/TaskStudentsManagement_final/version/entity/Courses.java
@@ -1,5 +1,10 @@
 package RaiseTechTask.TaskStudentsManagement_final.version.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Courses {
 
     private int id;
@@ -7,44 +12,4 @@ public class Courses {
     private String courseName;
     private String courseStartDay;
     private String courseCompletionDay;
-
-    public String getCourseStartDay() {
-        return courseStartDay;
-    }
-
-    public void setCourseStartDay(String courseStartDay) {
-        this.courseStartDay = courseStartDay;
-    }
-
-    public String getCourseCompletionDay() {
-        return courseCompletionDay;
-    }
-
-    public void setCourseCompletionDay(String courseCompletionDay) {
-        this.courseCompletionDay = courseCompletionDay;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
 }
--- src/main/java/RaiseTechTask/TaskStudentsManagement_final/version/entity/Students.java
+++ src/main/java/RaiseTechTask/TaskStudentsManagement_final/version/entity/Students.java
@@ -1,42 +1,14 @@
 package RaiseTechTask.TaskStudentsManagement_final.version.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Students {
 
     private String fullName;
     private int id;
     private int age;
     private String gender;
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
 }
