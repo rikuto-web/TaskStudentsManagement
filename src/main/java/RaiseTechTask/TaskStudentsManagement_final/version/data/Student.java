52a6aa8bbbd78045abf2456917b44f25bc8e214d
Shinya <rikutoweb5@gmail.com>
2025-05-15 23:44:40 +0900

    29_課題提出

--- src/main/java/RaiseTechTask/TaskStudentsManagement_final/version/data/Student.java
+++ src/main/java/RaiseTechTask/TaskStudentsManagement_final/version/data/Student.java
@@ -16,4 +16,5 @@ public class Student {
     private int age;
     private String gender;
     private String remark;
+    private boolean isDeletes;
 }
--- src/main/java/RaiseTechTask/TaskStudentsManagement_final/version/repository/StudentsRepository.java
+++ src/main/java/RaiseTechTask/TaskStudentsManagement_final/version/repository/StudentsRepository.java
@@ -12,18 +12,32 @@ public interface StudentsRepository {
     @Select("SELECT * FROM students")
     List<Student> selectAllStudent();
 
+    //ID取得
+    @Select("SELECT * FROM students WHERE id = #{id}")
+    Student selectStudentById(Integer id);
+
     //生徒の追加
     @Insert("INSERT INTO students (full_name, furigana, nickname, email_address, address, age, gender, remark)" +
             "VALUES(#{fullName}, #{furigana}, #{nickname}, #{emailAddress}, #{address}, #{age}, #{gender}, #{remark})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     void addStudent(Student student);
 
     //生徒の更新
-    @Update("UPDATE students SET full_name= #{full_name}, nickname= #{nickname}, email_address= #{email_address}," +
-            "address= #{address}, age= #{age}, gender=#{gender}" +
+    @Update("UPDATE students SET " +
+            "full_name= #{fullName}, " +
+            "furigana= #{furigana}," +
+            "nickname= #{nickname}, " +
+            "email_address= #{emailAddress}," +
+            "address= #{address}, " +
+            "age= #{age}, " +
+            "gender=#{gender}, " +
+            "remark=#{remark}" +
             "WHERE id = #{id}")
     void updateStudent(Student student);
 
     //生徒の削除
     @Delete("DELETE FROM students WHERE id = #{id}")
     void deleteStudent(int id);
+
+
 }
